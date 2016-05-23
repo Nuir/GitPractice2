ffa070f93ce46a1789c3862540cf879bff3dbce7
ldgeao99 <ldgeao99@naver.com>
2016-05-23 18:14:40 +0900

    //이대곤2

--- MFCApplication1/ChildView.cpp
+++ MFCApplication1/ChildView.cpp
@@ -48,7 +48,7 @@ void CChildView::OnPaint()
 	CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
 					   ///이대곤
 	// TODO: 여기에 메시지 처리기 코드를 추가합니다.
-	
+	//이대곤2
 	// 그리기 메시지에 대해서는 CWnd::OnPaint()를 호출하지 마십시오.
 }
 
